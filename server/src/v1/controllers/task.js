91bb6442328e5c7c130b9fb9a1d9eb6b1cd84ffe
iEasyJet <moma2108@mail.ru>
2024-09-15 12:58:48 +0300

    finish task api

--- server/src/v1/controllers/task.js
+++ server/src/v1/controllers/task.js
@@ -1,7 +1,7 @@
 const Task = require('../models/task');
 
 exports.createTask = async (req, res) => {
-  const { sectionId } = req.params;
+  const { sectionId } = req.body;
 
   try {
     const tasks = await Task.find({ section: sectionId });
@@ -31,10 +31,13 @@ exports.updateTask = async (req, res) => {
 };
 
 exports.deleteTask = async (req, res) => {
-  const { sectionId, taskId } = req.params;
+  const { taskId } = req.params;
   try {
+    const currentTask = await Task.findById(taskId);
     await Task.findByIdAndDelete(taskId);
-    const tasks = await Task.find({ section: sectionId }).sort('position');
+    const tasks = await Task.find({ section: currentTask.section }).sort(
+      'position'
+    );
 
     for (const key in tasks) {
       await Task.findByIdAndUpdate(tasks[key]._id, {
@@ -43,6 +46,44 @@ exports.deleteTask = async (req, res) => {
     }
 
     res.status(200).json('deleted');
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.updatePositionTask = async (req, res) => {
+  const {
+    resourceList,
+    destinationList,
+    resourceSectionId,
+    destinationSectionId,
+  } = req.body;
+
+  const resourceListReverse = resourceList.reverse();
+  const destinationListReverse = destinationList.reverse();
+
+  try {
+    if (resourceSectionId !== destinationSectionId) {
+      for (const key in resourceListReverse) {
+        await Task.findByIdAndUpdate(resourceListReverse[key]._id, {
+          $set: {
+            section: resourceSectionId,
+            position: key,
+          },
+        });
+      }
+    }
+
+    for (const key in destinationListReverse) {
+      await Task.findByIdAndUpdate(destinationListReverse[key]._id, {
+        $set: {
+          section: destinationSectionId,
+          position: key,
+        },
+      });
+    }
+
+    res.status(200).json('updated');
   } catch (error) {
     res.status(500).json(error);
   }
